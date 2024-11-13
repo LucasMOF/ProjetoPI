cf1cb18cea156b29634568e36a2500df7c6eaaba
Lucas Moura de Oliveira Felipe <20211204010003@ifrn.local>
2024-11-13 10:17:42 -0300

    Criando Controller do Projeto

---
+++ src/main/java/ifrn/pi/projeto/controllers/ProjetoControllers.java
@@ -0,0 +1,14 @@
+package ifrn.pi.projeto.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProjetoControllers {
+
+	@RequestMapping("/projeto/cadastro")
+	public String cadastro() {
+		return "cadastroProjeto";
+	}
+	
+}
--- src/main/java/ifrn/pi/projeto/models/Projeto.java
+++ src/main/java/ifrn/pi/projeto/models/Projeto.java
@@ -7,5 +7,45 @@ public class Projeto {
 	private String cpf;
 	private String telofe;
 	private String senha;
-	
+
+	public Long getNome() {
+		return nome;
+	}
+
+	public void setNome(Long nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelofe() {
+		return telofe;
+	}
+
+	public void setTelofe(String telofe) {
+		this.telofe = telofe;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
