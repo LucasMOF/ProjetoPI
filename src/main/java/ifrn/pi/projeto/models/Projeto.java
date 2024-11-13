cdb9d3d8e73897616cb14f15d07ef9b848c947fd
Lucas Moura de Oliveira Felipe <20211204010003@ifrn.local>
2024-11-13 10:13:35 -0300

    Projeto criado (Evento)

---
+++ src/main/java/ifrn/pi/projeto/models/Projeto.java
@@ -0,0 +1,11 @@
+package ifrn.pi.projeto.models;
+
+public class Projeto {
+
+	private Long nome;
+	private String email;
+	private String cpf;
+	private String telofe;
+	private String senha;
+	
+}
